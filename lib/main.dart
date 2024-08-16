245228e8bf054704086716e7fa50b28da46989f8
Aaditya Shah <lizmeestore@gmail.com>
2024-08-16 13:27:13 +0545

    one time onboarding implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_ui_marry/dash_board/features/auth/google_auth/sign_in_page.dart';
 
 import 'package:flutter_ui_marry/dash_board/features/user_data/presentation/page/onbaring_screen.dart';
-
+import 'package:hive_flutter/hive_flutter.dart';
+import 'dash_board/features/home/presentation/pages/homepage.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,11 +13,57 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await Hive.initFlutter();
+  await Hive.openBox<bool>('isFirstTimeUser');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Box<bool> userBox;
+  late bool _isFirstTimeUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    try {
+      _isFirstTimeUser = true;
+      userBox = Hive.box<bool>('isFirstTimeUser');
+
+      checkOnboardingStatus();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void checkOnboardingStatus() {
+    try {
+      bool isFirstTimeUser = userBox.get('isFirstTimeUser', defaultValue: true)!;
+      setState(() {
+        _isFirstTimeUser = isFirstTimeUser;
+      });
+    } catch (e) {
+      debugPrint('Error checking onboarding status: $e');
+    }
+  }
+
+  void completeOnboarding() {
+    try {
+      userBox.put('isFirstTimeUser', false);
+      setState(() {
+        _isFirstTimeUser = false;
+      });
+    } catch (e) {
+      debugPrint('Error completing onboarding: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,55 +72,20 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       builder: (context, child) {
         return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Marry Me',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          // Define the routes here
-          routes: {
-            '/home': (context) => HomePage(), // Ensure HomePage is defined
-            '/signIn': (context) => SignInPage(),
-            '/onboarding': (context) =>
-                OnboardingFlow(), // Register the OnboardingFlow route
-          },
-          // Set the initial route depending on user onboarding status
-          home: FutureBuilder(
-            future:
-                _checkIfOnboardingCompleted(), // Method to check onboarding status
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                // If onboarding is completed, show sign-in page; otherwise, start onboarding
-                return snapshot.data == true ? SignInPage() : OnboardingFlow();
-              }
+            debugShowCheckedModeBanner: false,
+            title: 'Marry Me',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            // Define the routes here
+            routes: {
+              '/home': (context) => HomePage(), // Ensure HomePage is defined
+              '/signIn': (context) => SignInPage(),
+              '/onboarding': (context) => OnboardingFlow(), // Register the OnboardingFlow route
             },
-          ),
-        );
+            // Set the initial route depending on user onboarding status
+            home: _isFirstTimeUser ? OnboardingFlow(completeOnboarding: completeOnboarding) : SignInPage());
       },
-    );
-  }
-
-  // This method checks whether the user has completed the onboarding process
-  Future<bool> _checkIfOnboardingCompleted() async {
-    // Implement logic to check if onboarding is completed
-    // For example, check shared preferences or a Firestore flag
-    // Returning false for now as an example
-    return false;
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
-      ),
-      body: Center(
-        child: Text("Welcome to the Home Page!"),
-      ),
     );
   }
 }
